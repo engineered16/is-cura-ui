507eef4631ef6c2d967ad1f6007989f982a3b5f5
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2019-11-09 23:05:08 +0100

    SmartSliceCloudConnector: Building and adding job metadata with pywim
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com> (github: thopiekar)

--- SmartSlicePlugin/SmartSliceExtension/SmartSliceCloudConnector.py
+++ SmartSlicePlugin/SmartSliceExtension/SmartSliceCloudConnector.py
@@ -6,11 +6,12 @@ Created on 22.10.2019
 
 import time
 import os
+import uuid
 import tempfile
 import json
 import zipfile
 
-import pywim
+import pywim  # @UnresolvedImport
 
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import pyqtSlot
@@ -56,13 +57,18 @@ class ConnectivityChecker(QObject):
 class SmartSliceCloudVerificationJob(Job):
     # This job is responsible for uploading the backup file to cloud storage.
     # As it can take longer than some other tasks, we schedule this using a Cura Job.
+    
     def __init__(self, connector) -> None:
         super().__init__()
         self.connector = connector
+        
+        self.job_type = pywim.smartslice.job.JobType.validation
 
     def run(self) -> None:
         # TODO: Add instructions how to send a verification job here
-        time.sleep(5)
+        
+        self.connector.sendJob(self.job_type)
+
         if not self.connector._demo_was_underdimensioned_before:
             self.connector.status = SmartSliceCloudStatus.Underdimensioned
             self.connector._demo_was_underdimensioned_before = True
@@ -288,40 +294,43 @@ class SmartSliceCloudConnector(QObject):
     def variables(self):
         return self.extension.getVariables(None, None)
 
-    def prepareInitial3mf(self, location=None):
-        # Using tempfile module to probe for a temporary file path
-        # TODO: We can do this more elegant of course, too.
-        if not location:
-            file = tempfile.NamedTemporaryFile(suffix=".3mf")
-            location = file.name
-            del(file)
-            Logger.log("d", "location: {}".format(location))
-
-        # Checking whether count of models == 1
-        mesh_nodes = self.getSliceableNodes()
-        if len(mesh_nodes) is not 1:
-            Logger.log("d", "Found {} meshes!".format(["no", "too many"][len(mesh_nodes) > 1]))
-            return None
-
+    def prepareInitial3mf(self, threemf_path, mesh_nodes):
         # Getting 3MF writer and write our file
         threeMF_Writer = PluginRegistry.getInstance().getPluginObject("3MFWriter")
-        threeMF_Writer.write(location, mesh_nodes)
+        threeMF_Writer.write(threemf_path, mesh_nodes)
 
-        return location
+        return True
 
-    def appendTo3mf(self, filename, node):
+    def extend3mf(self, filepath, mesh_nodes, job_type):
+        # NOTE: As agreed during the POC, we want to analyse and optimize only one model at the moment.
+        #       The lines below will partly need to be executed as "for model in models: bla bla.."
+        mesh_node = mesh_nodes[0]
+        
+        mesh_node_stack = mesh_node.callDecoration("getStack")
+        
         machine_manager = Application.getInstance().getMachineManager()
         active_machine = machine_manager.activeMachine
         
-        active_extruder = node.callDecoration("getActiveExtruderPosition")
+        active_extruder_position = mesh_node.callDecoration("getActiveExtruderPosition")
+        if active_extruder_position is None:
+            active_extruder_position = 0
+        else:
+            active_extruder_position = int(active_extruder_position)
+        
+        
+        active_extruder = active_machine.extruders[str(active_extruder_position)]
         
         extruders = list(active_machine.extruders.values())
-        extruders = sorted(extruders, key = lambda extruder: extruder.getMetaDataEntry("position"))
+        extruders = sorted(extruders,
+                           key = lambda extruder: extruder.getMetaDataEntry("position")
+                           )
 
-        material_guids_per_extruder = [extruder.material.getMetaData().get("GUID", "") for extruder in extruders]
+        material_guids_per_extruder = []
+        for extruder in extruders:
+            material_guids_per_extruder.append(extruder.material.getMetaData().get("GUID", ""))
 
         # TODO: Needs to be determined from the used model
-        guid = material_guids_per_extruder[active_extruder]
+        guid = material_guids_per_extruder[active_extruder_position]
         
         # Determine material properties from material database
         this_dir = os.path.split(__file__)[0]
@@ -336,18 +345,19 @@ class SmartSliceCloudConnector(QObject):
                 break
         
         if not material_found:
-            return None
+            # TODO: Alternatively just raise an exception here
+            return False
         
         job = pywim.smartslice.job.Job()
 
-        job.type = pywim.smartslice.job.JobType.optimization
+        job.type = job_type
     
         # Create the bulk material definition. This likely will be pre-defined
         # in a materials database or file somewhere
         job.bulk = pywim.fea.model.Material(name=material_found["name"])
         job.bulk.density = material_found["density"]
-        job.bulk.elastic = pywim.fea.model.Elastic(properties={'E': material_found['E'],
-                                                               'nu': material_found['v']})
+        job.bulk.elastic = pywim.fea.model.Elastic(properties={'E': material_found["elastic"]['E'],
+                                                               'nu': material_found["elastic"]['v']})
     
         # Setup optimization configuration
         job.optimization.min_safety_factor = self.variables.safetyFactor
@@ -386,7 +396,7 @@ class SmartSliceCloudConnector(QObject):
         # Set the components on the force vector. In this example
         # we have 100 N, 200 N, and 50 N in the x, y, and z
         # directions respectfully.
-        force1.force.set((100., 200., 50.))
+        force1.force.set((100.,200., 50.))
     
         # Add the face Ids from the STL mesh that the user selected for
         # this force
@@ -406,36 +416,133 @@ class SmartSliceCloudConnector(QObject):
         # Now we need to setup the print/slicer configuration
         
         print_config = pywim.am.Config()
-        print_config.layer_width = 0.45
+        print_config.layer_width = active_machine.getProperty("line_width", "value")
+        print_config.layer_height = active_machine.getProperty("layer_height", "value")
+        print_config.walls = mesh_node_stack.getProperty("wall_line_count", "value")
+        
+        # skin angles - CuraEngine vs. pywim
+        # > https://github.com/Ultimaker/CuraEngine/blob/master/src/FffGcodeWriter.cpp#L402 
+        skin_angles = active_machine.getProperty("skin_angles", "value")
+        if type(skin_angles) is str:
+            skin_angles = eval(skin_angles)
+        if len(skin_angles) > 0:
+            print_config.skin_orientations.extend(tuple(skin_angles))
+        else:
+            print_config.skin_orientations.extend((45, 135))
+        
+        
+        print_config.bottom_layers = mesh_node_stack.getProperty("top_layers", "value")
+        print_config.top_layers = mesh_node_stack.getProperty("bottom_layers", "value")
+        
+        # infill pattern - Cura vs. pywim 
+        infill_pattern = mesh_node_stack.getProperty("infill_pattern", "value")
+        infill_pattern_to_pywim_dict = {"grid": pywim.am.InfillType.grid,
+                                        "triangles": pywim.am.InfillType.triangle,
+                                        "cubic": pywim.am.InfillType.cubic
+                                        }
+        if infill_pattern in infill_pattern_to_pywim_dict.keys():
+            print_config.infill.pattern = infill_pattern_to_pywim_dict[infill_pattern]
+        else:
+            print_config.infill.pattern = pywim.am.InfillType.unknown
+        
+        print_config.infill.density = mesh_node_stack.getProperty("infill_sparse_density", "value")
+        
+        # infill_angles - Setting defaults from the CuraEngine
+        # > https://github.com/Ultimaker/CuraEngine/blob/master/src/FffGcodeWriter.cpp#L366
+        infill_angles = mesh_node_stack.getProperty("infill_angles", "value")
+        if type(infill_angles) is str:
+            infill_angles = eval(infill_angles)
+        #print("infill_angles: {}".format(infill_angles))
+        if not len(infill_angles):
+            # Check the URL below for the default angles. They are infill type depended.
+            print_config.infill.orientation = 45
+        else:
+            if len(infill_angles) > 1:
+                Logger.log("w", "More than one infill angle is set! Only the first will be taken!")
+                Logger.log("d", "Ignoring the angles: {}".format(infill_angles[1:]))
+            print_config.infill.orientation = infill_angles[0]
         # ... and so on, check pywim.am.Config for full definition
     
         # The am.Config contains an "auxiliary" dictionary which should
         # be used to define the slicer specific settings. These will be
         # passed on directly to the slicer (CuraEngine).
-        print_config.auxiliary['print_bed_temperature'] = '60.0'
+        has_machine_heated_bed = active_machine.getProperty("machine_heated_bed", "value")
+        if has_machine_heated_bed:
+            print_config.auxiliary['print_bed_temperature'] = active_extruder.getProperty("material_bed_temperature",
+                                                                                          "value")
+        else:
+            print_config.auxiliary['print_bed_temperature'] = 0
     
         # Setup the slicer configuration. See each class for more
         # information.
-        extruder0 = pywim.chop.machine.Extruder(diameter=0.4)
-        printer = pywim.chop.machine.Printer(name=active_machine.getName(), extruders=(extruder0, ))
+        extruder0 = pywim.chop.machine.Extruder(diameter=active_extruder.getProperty("machine_nozzle_size",
+                                                                                     "value"))
+        printer = pywim.chop.machine.Printer(name=active_machine.getName(),
+                                             extruders=(extruder0,
+                                                        )
+                                             )
     
         # And finally set the slicer to the Cura Engine with the config and printer defined above
-        job.chop.slicer = pywim.chop.slicer.CuraEngine(config=print_config, printer=printer)
+        job.chop.slicer = pywim.chop.slicer.CuraEngine(config=print_config,
+                                                       printer=printer)
     
-        with zipfile.ZipFile(filename, 'a') as tmf:
-            tmf.writestr('SmartSlice/job.json', job.to_json())
+        threemf_file = zipfile.ZipFile(filepath, 'a')
+        threemf_file.writestr('SmartSlice/job.json',
+                              job.to_json()
+                              )
         
 
-        return filename
+        return True
 
-    def sendJob(self):
+    def determineTempDirectory(self):
+        temporary_directory = tempfile.gettempdir()
+        base_subdirectory_name = "smartslice"
+        private_subdirectory_name = base_subdirectory_name
+        abs_private_subdirectory_name = os.path.join(temporary_directory,
+                                                     private_subdirectory_name)
+        private_subdirectory_suffix_num = 1
+        while os.path.exists(abs_private_subdirectory_name) and not os.path.isdir(abs_private_subdirectory_name):
+            private_subdirectory_name = "{}_{}".format(base_subdirectory_name,
+                                                       private_subdirectory_suffix_num)
+            abs_private_subdirectory_name = os.path.join(temporary_directory,
+                                                         private_subdirectory_name)
+            private_subdirectory_suffix_num += 1
+        
+        if not os.path.exists(abs_private_subdirectory_name):
+            os.makedirs(abs_private_subdirectory_name)
+        
+        return abs_private_subdirectory_name
+        
+
+    # Sending jobs to AWS
+    # - jtype: Job type to be sent. Can be either:
+    #          > pywim.smartslice.job.JobType.validation
+    #          > pywim.smartslice.job.JobType.optimization
+    def sendJob(self, jtype):
+        # Using tempfile module to probe for a temporary file path
+        # TODO: We can do this more elegant of course, too.
+        
+        # Setting up file output
+        filename = "{}.3mf".format(uuid.uuid1())
+        filedir = self.determineTempDirectory()
+        filepath = os.path.join(filedir, filename)
+        
+        Logger.log("d", "Saving temporary (and custom!) 3MF file at: {}".format(filepath))
+
+        # Checking whether count of models == 1
+        mesh_nodes = self.getSliceableNodes()
+        if len(mesh_nodes) is not 1:
+            Logger.log("d", "Found {} meshes!".format(["no", "too many"][len(mesh_nodes) > 1]))
+            return None
+
         Logger.log("d", "Sending slice job!")
-        filename = self.prepareInitial3mf()
+        self.prepareInitial3mf(filepath, mesh_nodes)
+        self.extend3mf(filepath,
+                       mesh_nodes,
+                       jtype)
 
         if not filename:
             return None
-
-        self.appendTo3mf(filename)
 
         # SEND YOUR 3MF TO THE CLOUD HERE
 
