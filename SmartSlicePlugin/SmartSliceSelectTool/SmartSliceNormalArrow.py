e76049964b6abdb75d2d1a2ea200bd24d6b42302
Cody Fagley <cfagley@codeus.tech>
2019-11-11 14:13:14 -0700

    v19.0.5.0: Added basic properties and accessors/mutators

--- SmartSlicePlugin/SmartSliceSelectTool/SmartSliceNormalArrow.py
+++ SmartSlicePlugin/SmartSliceSelectTool/SmartSliceNormalArrow.py
@@ -29,6 +29,21 @@ class SmartSliceNormalArrow(QObject):
         super().__init__()
         
         self._parent = parent
+
+        #  Rotations (Euler Angles)
+        self._rot_x = 0.
+        self._rot_y = 0.
+        self._rot_z = 0.
+
+        #  Translation Coordinates
+        self._coord_x = 0.
+        self._coord_y = 0.
+        self._coord_z = 0.
+
+        #  Normal Vector Components (DEPREPECATED)
+        self._norm_x = 0.
+        self._norm_y = 0.
+        self._norm_z = 0.
         
         #  If SelectableFace is provided, draw the Normal Arrow
         if parent is not None:
@@ -37,8 +52,69 @@ class SmartSliceNormalArrow(QObject):
 
 #  ACCESSORS
 
+    '''
+      rotations()
+        Returns 3-wide list of euler angles (alpha, beta, gamma)
+    '''
+    @property
+    def rotations(self):
+        return [self._rot_x, self._rot_y, self._rot_z]
+
+    '''
+      coordinates()
+        Returns 3-wide list of (x, y, z) positional coordinates
+    '''
+    @property
+    def coordinates(self):
+        return [self._coord_x, self._coord_y, self._coord_z]
+
+    '''
+      normals()
+        Returns 3-wide list of (x, y, z) normal unit vectors
+    '''
+    @property
+    def normals(self):
+        return [self._norm_x, self._norm_y, self._norm_z]
 
 #  MUTATORS
+
+    '''
+      rotations(rots)
+        rots : list of floats
+
+        Accepts 3-wide list of floats ('rots') and sets corresponding rotational angles
+    '''
+    @rotations.setter
+    def rotations(self, rots):
+        self._rot_x = rots[0]
+        self._rot_y = rots[1]
+        self._rot_z = rots[2]
+
+    '''
+      coordinates(coords)
+        coords : list of floats
+
+        Accepts 3-wide list of floats ('coords') and sets corresponding cartesian coordinates
+    '''
+    @coordinates.setter
+    def coordinates(self, coords):
+        self._coords_x = coords[0]
+        self._coords_y = coords[1]
+        self._coords_z = coords[2]
+
+    '''
+      normals(norms)
+        norms: list of floats
+
+        Accepts 3-wide list of floats ('norms') and sets corresponding normal vector components
+    '''
+    @normals.setter
+    def normals(self, norms):
+        self._norm_x = norms[0]
+        self._norm_y = norms[1]
+        self._norm_z = norms[2]
+
+
     '''
       drawNormalArrow()
     '''
