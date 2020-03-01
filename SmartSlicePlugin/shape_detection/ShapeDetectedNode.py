5ea788c2c2b45dfa5eeb1d5c49bed500f5775679
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2020-03-01 20:56:59 +0100

    WIP
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com> (github: thopiekar)

--- SmartSlicePlugin/shape_detection/ShapeDetectedNode.py
+++ SmartSlicePlugin/shape_detection/ShapeDetectedNode.py
@@ -17,6 +17,7 @@ from CGAL.CGAL_Shape_detection import efficient_RANSAC
 
 
 class ShapeDetectedNode():
+    plastic_number = 1.324717957244746025960908854
     shape_index_name = "shape_index"
 
     def __init__(self, node):
@@ -43,16 +44,9 @@ class ShapeDetectedNode():
         Logger.log("d", "Processing {} faces.".format(len(face_ids)))
         for face_id in face_ids:
             vector_a, vector_b, vector_c = self.mesh.getFaceNodes(face_id)
-            vector_centroid = (vector_a + vector_b + vector_c) / 3.0
-            surface_point_to_centroid_weight = 0.5
-
-            # Preparing our surface points on a face
-            surface_point_a = (vector_centroid - vector_a) * \
-                surface_point_to_centroid_weight
-            surface_point_b = (vector_centroid - vector_b) * \
-                surface_point_to_centroid_weight
-            surface_point_c = (vector_centroid - vector_c) * \
-                surface_point_to_centroid_weight
+            print("vector_a", vector_a)
+            print("vector_b", vector_b)
+            print("vector_c", vector_c)
 
             normal_vector = numpy.cross(vector_b - vector_a,
                                         vector_c - vector_a
@@ -63,13 +57,29 @@ class ShapeDetectedNode():
             # Normalize vector as the direction matters only
             normal = normal / math.sqrt(normal.squared_length())
 
-            for surface_point in (surface_point_a,
-                                  surface_point_b,
-                                  surface_point_c):
-                x, y, z = [float(entry) for entry in surface_point.tolist()]
-                point = Point_3(x, y, z)
+            surface_points = []
+            vector_ab = vector_b - vector_a
+            vector_ac = vector_c - vector_a
+            print("vector_ab", vector_ab)
+            print("vector_ac", vector_ac)
+            for point_n in range(1, 201):
+                a1 = 1.0 / self.plastic_number
+                a2 = (1.0 / self.plastic_number)**2
+                r_1 = (0.5 + a1 * point_n) % 1
+                r_2 = (0.5 + a2 * point_n) % 1
+                if (r_1 + r_2) <= 1:
+                    vector_p = vector_ac * r_1 + vector_ab * r_2
+                else:
+                    vector_p = vector_ac * (1 - r_1) + vector_ab * (1 - r_2)
+                vector_p += vector_a
+                print("vector_p", vector_p)
+                print("normal", normal)
+                point_p = [float(entry) for entry in vector_p.tolist()]
+                point_p = Point_3(*point_p)
+                surface_points.append(point_p)
 
-                self._point_cloud.insert(point, normal)
+            for surface_point in surface_points:
+                self._point_cloud.insert(surface_point, normal)
                 if face_id not in self._point_face.keys():
                     self._point_face[face_id] = ()
                 self._point_face[face_id] += (surface_point, )
@@ -79,18 +89,20 @@ class ShapeDetectedNode():
             cloud = self.point_cloud
 
         Logger.log("d", "Running efficient_RANSAC...")
+        Logger.log("d", "Inspecting {} points...".format(self.point_cloud.number_of_points()))
         shape_map = self._point_cloud.int_map(self.shape_index_name)
         shapes = efficient_RANSAC(self._point_cloud,
                                   shape_map,
+                                  # probability=5,  # [%]
                                   min_points=5,
-                                  epsilon=1.,
-                                  cluster_epsilon=1.2,
-                                  normal_threshold=0.85,
+                                  # epsilon=1.,
+                                  # normal_threshold=0.85,
+                                  # cluster_epsilon=1.2,
                                   planes=True,
                                   cylinders=True,
-                                  spheres=True,
-                                  cones=True,
-                                  tori=True
+                                  spheres=False,
+                                  cones=False,
+                                  tori=False
                                   )
         print(len(shapes), "shapes(s) detected, first 10 shapes are:")
         for s in range(min(len(shapes), 10)):
